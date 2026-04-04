d97b19a6c61c475d4270133b15168031bdbcc0b9
Ivan <ialiagaa@unc.edu.pe>
2026-04-04 18:35:38 -0500

    update fire

--- src/firebase.js
+++ src/firebase.js
@@ -3,12 +3,12 @@ import { getFirestore } from "firebase/firestore";
 
 // 👇 pega tu config aquí
 const firebaseConfig = {
-  apiKey: "AIzaSyD7q47fOzRu8RmldRIJdriJ_tLA5GoFbq0",
-  authDomain: "asistencia-ers.firebaseapp.com",
-  projectId: "asistencia-ers",
-  storageBucket: "asistencia-ers.firebasestorage.app",
-  messagingSenderId: "293780124942",
-  appId: "1:293780124942:web:48c3d7d3c608f3e6d9535f"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
 };
 
 // Initialize Firebase
