b4c2bdffa9e0ce76ee2c78afe6eea22b59c16fba
Ivan <ialiagaa@unc.edu.pe>
2026-04-05 16:40:27 -0500

    Add pwa

--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,10 @@ import App from "./App";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./styles.css";
 
+import { registerSW } from 'virtual:pwa-register'
+
+registerSW({ immediate: true })
+
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <App />
--- vite.config.js
+++ vite.config.js
@@ -1,17 +1,37 @@
-// import { defineConfig } from 'vite'
-// import react from '@vitejs/plugin-react'
-
-// // https://vite.dev/config/
-// export default defineConfig({
-//   plugins: [react()],
-// })
-
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { VitePWA } from 'vite-plugin-pwa'
 
 export default defineConfig({
-  plugins: [react()],
-  server: {
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      includeAssets: ['icon.png'],
+      manifest: {
+        name: 'Asistencia ERS',
+        short_name: 'ERS',
+        description: 'Control de asistencia',
+        theme_color: '#0d6efd',
+        background_color: '#ffffff',
+        display: 'standalone',
+        start_url: '/',
+        icons: [
+          {
+            src: '/icon.png',
+            sizes: '192x192',
+            type: 'image/png'
+          },
+          {
+            src: '/icon.png',
+            sizes: '512x512',
+            type: 'image/png'
+          }
+        ]
+      }
+    }),
+  ],
+   server: {
     host: true
   }
 })
