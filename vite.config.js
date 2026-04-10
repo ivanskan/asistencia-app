989b5c3b35b0c3f9f56389e6b185cc14d7c549e7
Ivan <ialiagaa@unc.edu.pe>
2026-04-09 23:22:54 -0500

    iph add conf

--- vite.config.js
+++ vite.config.js
@@ -8,7 +8,7 @@ export default defineConfig({
     VitePWA({
       registerType: 'autoUpdate',
       manifest: {
-        name: 'Asistencia ERS',
+        name: 'PK CORP',
         short_name: 'Asistencia',
         description: 'Control de asistencia con QR',
         theme_color: '#0d6efd',
