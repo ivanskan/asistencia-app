7cfb2ce340886aba29dd1f2f95f120377fd3b836
ivan <ialiagaa@unc.edu.pe>
2026-08-03 15:40:28 -0500

    api

--- src/services/api.js
+++ src/services/api.js
@@ -1,13 +1,21 @@
-//dev
+////dev
+
+// const API = "/Sistema/Api";
 
 
-const API = "/Sistema/Api";
-
-
-// prod
+//// prod
 
 // const API = "https://ersperu.pe/Sistema/Api";
+
+// export default API;
+
+
+
+////////////// Config to dev and prod /////////////////////
+
+const API = import.meta.env.VITE_API_URL;
 
 export default API;
 
 
+///////////////////////////////////////////////////////////
